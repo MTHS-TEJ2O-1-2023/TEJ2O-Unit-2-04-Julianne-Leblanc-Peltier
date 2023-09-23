cb13ee4299cab313594d858053de4cfb7e6d6ff1
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2023-09-23 03:38:21 +0000

    python vers.

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,7 +1,8 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Julianne Leblanc-Peltier
+Created on: Sep 2023
 This module is a Micro:bit MicroPython program
+This code displays the current temperature on a MicroBit
 """
 
 from microbit import *
